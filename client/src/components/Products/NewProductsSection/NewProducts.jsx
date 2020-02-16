98677776f3da245fc72230befdd0080d4b080aec
Damola Adewunmi <damolasd@gmail.com>
2020-02-16 09:29:41 +0100

    Added text

--- client/src/components/Products/NewProductsSection/NewProducts.jsx
+++ client/src/components/Products/NewProductsSection/NewProducts.jsx
@@ -5,7 +5,7 @@ export default () => {
     return (
         <section className="wn__product__area brown--color pt--80  pb--30">
             <div className="container">
-                <TopHeading Title="All" Heading="PRODUCTS" Paragraph="There are many variations of passages of Lorem Ipsum available, but the majority have suffered lebmid alteration in some ledmid form" />
+                <TopHeading Title="All" Heading="PRODUCTS" Paragraph="From art, history, science, mathematics and so much more. You can find books on a range of topics here" />
                 <NewProductCarousel />
             </div>
         </section>
